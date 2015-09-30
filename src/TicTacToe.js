116d8b23f4c72d8a6708a99ce08e7394dacfcd1b
Imogen <imogen.wentworth@gmail.com>
2015-09-29 17:52:37 -0700

    Add TicTacToe component

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,27 +1,73 @@
 import React from 'react';
+import Board from './Board';
 
 'use strict';
 
 class TicTacToe extends React.Component {
-  getRow(count) {
-    let cells = [];
-    for (var i = 0; i < this.props.xAxis; i++) {
-      cells.push(<div className="cell" key={ `cell-${count}-${i}` }></div>);
-    }
-    return cells;
+
+  constructor(props) {
+    super(props);
+    this.board = new Board(props.width);
+    this.state = {
+      player: Math.floor(Math.random() * 2) + 1,
+      freezeBoard: false
+    };
   }
 
-  getGrid() {
-    let rows = [];
-    for (var i = 0; i < this.props.yAxis; i++) {
-      rows.push(<div className="row" key={ `row-${i}` }>{ this.getRow(i) }</div>);
+  aiMove() {
+    this.setState({
+      player: this.state.player === 1 ? 2 : 1,
+      freezeBoard: true
+    });
+    // Trigger AI
+    // this.board.move(x, y, this.state.player);
+    this.setState({
+      player: this.state.player === 1 ? 2 : 1,
+      freezeBoard: false
+    });
+  }
+
+  switchPlayer() {
+    this.setState({player: this.state.player === 1 ? 2 : 1});
+  }
+
+  handleMove(event) {
+    let [x, y] = event.target.dataset.cell.split('_');
+    let cellEmpty = this.board.getCell(x, y) === 0;
+
+    if (cellEmpty) {
+      this.board.move(x, y, this.state.player);
+
+      if (this.props.singlePlayer) {
+        this.aiMove();
+      } else {
+        this.switchPlayer();
+      }
     }
-    return rows;
   }
 
   render() {
-    return <div className="grid">{ this.getGrid() }</div>;
+    let { board } = this.board;
+
+    let grid = board.map((row, rowInd) => {
+      let cells = row.map((cell, cellInd) => {
+        let classString = cell > 0 ? cell === 1 ? 'cell-p1' : 'cell-p2' : 'cell';
+        let coords = `${rowInd}_${cellInd}`;
+        let clickHandler;
+
+        if (!this.state.freezeBoard) { clickHandler = this.handleMove.bind(this); }
+
+        return <div className={ classString } key={ cellInd } onClick={ clickHandler } data-cell={ coords }></div>;
+      });
+
+      return <div className="row" key={ rowInd }>{ cells }</div>;
+    });
+
+    return <div className="grid">{ grid }</div>;
   }
 }
+
+TicTacToe.propTypes = { width: React.PropTypes.number };
+TicTacToe.defaultProps = { width: 3 };
 
 export default TicTacToe;
---
+++ test/specs/TicTacToe.spec.js
@@ -0,0 +1,116 @@
+import TicTacToe from '../../src/TicTacToe';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import { assert, expect } from 'chai';
+
+'use strict';
+
+describe('TicTacToe component', () => {
+
+  let component;
+
+  beforeEach(() => {
+    component = TestUtils.renderIntoDocument(<TicTacToe width={ 4 }/>);
+  });
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(ReactDOM.findDOMNode(component).parentNode);
+  });
+
+  it('should exist', () => {
+    assert.ok(TicTacToe);
+  });
+
+  describe('when rendered successfully', () => {
+
+    it('should have a board', () => {
+      expect(component).to.have.ownProperty('board');
+      assert.isArray(component.board.board);
+    });
+
+    it('should pick the first player at random', () => {
+      expect(component.state.player).to.be.within(1, 2);
+    });
+
+    it('should not be frozen initially', () => {
+      expect(component.state.freezeBoard).to.be.false;
+    });
+
+    it('should contain a grid', () => {
+      const grid = TestUtils.findRenderedDOMComponentWithClass(component, 'grid');
+      assert.ok(grid);
+    });
+
+    it('should contain the correct board elements', () => {
+      const rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'row');
+      const cells = TestUtils.scryRenderedDOMComponentsWithClass(component, 'cell');
+      expect(rows).to.have.length(4);
+      expect(cells).to.have.length(16);
+    });
+  });
+
+  describe('cells', () => {
+
+    it('should store the correct coords on the data attribute', () => {
+      const cell = TestUtils.scryRenderedDOMComponentsWithClass(component, 'cell')[0];
+      expect(cell.dataset.cell).to.equal('0_0');
+    });
+
+    it('should respond to click events', () => {
+      const cell = TestUtils.scryRenderedDOMComponentsWithClass(component, 'cell')[0];
+      component.setState({player: 1});
+      expect(component.board.getCell(0, 0)).to.equal(0);
+      TestUtils.Simulate.click(cell);
+      expect(component.board.getCell(0, 0)).to.equal(1);
+      expect(cell.classList[0]).to.equal('cell-p1');
+    });
+
+    it('should not respond to click events if frozen', () => {
+      component.setState({freezeBoard: true});
+      const cell = TestUtils.scryRenderedDOMComponentsWithClass(component, 'cell')[0];
+      component.setState({player: 1});
+      expect(component.board.getCell(0, 0)).to.equal(0);
+      TestUtils.Simulate.click(cell);
+      expect(component.board.getCell(0, 0)).to.equal(0);
+      expect(cell.classList[0]).to.equal('cell');
+    });
+  });
+
+  describe('switchPlayer', () => {
+
+    it('should toggle between players', () => {
+      component.setState({player: 1});
+      component.switchPlayer();
+      expect(component.state.player).to.equal(2);
+      component.switchPlayer();
+      expect(component.state.player).to.equal(1);
+    });
+  });
+
+  describe('handleMove', () => {
+
+    let mockEvent = {target: {dataset: {cell: '0_0'}}};
+
+    it('should populate an empty cell on the board', () => {
+      let { board } = component.board;
+      component.setState({player: 1});
+      component.handleMove(mockEvent);
+      expect(board[0][0]).to.equal(1);
+    });
+
+    it('should do nothing if cell is not empty', () => {
+      let { board } = component.board;
+      component.setState({player: 1});
+      board[0][0] = 2;
+      component.handleMove(mockEvent);
+      expect(board[0][0]).to.equal(2);
+    });
+
+    it('should switch player', () => {
+      component.setState({player: 1});
+      component.handleMove(mockEvent);
+      expect(component.state.player).to.equal(2);
+    });
+  });
+});
