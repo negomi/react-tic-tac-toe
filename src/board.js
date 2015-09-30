d32de9e3856051265970a439bbcbf9f8927e16fc
Imogen <imogen.wentworth@gmail.com>
2015-09-29 17:52:05 -0700

    Add Board service

--- src/board.js
+++ src/board.js
@@ -1,1 +1,112 @@
-export default {};
+'use strict';
+
+class Board {
+  constructor(width) {
+    if (typeof width !== 'number') {
+      throw new Error('Width must be a number');
+    }
+
+    if (width < 3 || width > 10) {
+      throw new Error('Width cannot be less than 3 or greater than 10');
+    }
+
+    this.board = [];
+
+    for (let i = 0; i < width; i++) {
+      let row = [];
+      for (let j = 0; j < width; j++) {
+        row.push(0);
+      }
+      this.board.push(row);
+    }
+  }
+
+  // Return the value of a given cell.
+  getCell(x, y) {
+    return this.board[x][y];
+  }
+
+  // Assign a cell on the board to a given player.
+  move(x, y, player) {
+    this.board[x][y] = player;
+  }
+
+  // Return the coordinates of all empty cells.
+  getEmptyCells() {
+    return this.board.map((row, rowInd) => {
+      return row.map((cell, cellInd) => {
+        if (cell === 0) {
+          return [rowInd, cellInd];
+        }
+      });
+    }).reduce((a, b) => {
+      return a.concat(b);
+    }).filter((cell) => {
+      return typeof cell !== 'undefined';
+    });
+  }
+
+  // Check if all values in an array are equal.
+  static allEqual(arr) {
+    return !arr.some((val, i, arr) => {
+      return val !== arr[0];
+    });
+  }
+
+  // Check if the game has been won.
+  // Returns player number, 3 in the case of a tie, or false if no winner.
+  checkWin() {
+    let winner = false;
+    let boardWidth = this.board.length;
+
+    // Check rows.
+    for (let rowInd = 0; rowInd < boardWidth; rowInd++) {
+      let row = this.board[rowInd];
+      if (Board.allEqual(row) && row[0] > 0) {
+        winner = row[0];
+        return winner;
+      }
+    }
+
+    // Check columns.
+    for (let rowInd = 0; rowInd < boardWidth; rowInd++) {
+      let col = [];
+      for (let colInd = 0; colInd < boardWidth; colInd++) {
+        col.push(this.getCell(colInd, rowInd));
+      }
+      if (Board.allEqual(col) && col[0] > 0) {
+        winner = col[0];
+        return winner;
+      }
+    }
+
+    // Check diagonals.
+    let diags = { left: [], right: [] };
+    for (let i = 0; i < boardWidth; i++) {
+      diags.left.push(this.getCell(i, i));
+    }
+    for (let i = boardWidth - 1; i >= 0; i--) {
+      diags.right.push(this.getCell(i, i));
+    }
+
+    for (let key in diags) {
+      let diag = diags[key];
+      if (Board.allEqual(diag) && diag[0] > 0) {
+        winner = diag[0];
+        return winner;
+      }
+    }
+
+    // Check tie.
+    let tie = !this.board.reduce((a, b) => {
+      return a.concat(b);
+    }).some((cell) => {
+      return cell === 0;
+    });
+    if (tie) winner = 3;
+
+    return winner;
+  }
+}
+
+export default Board;
--- test/specs/board.spec.js
+++ test/specs/board.spec.js
@@ -1,8 +1,76 @@
-import board from '../../src/board';
-import { expect, should } from 'chai';
+import Board from '../../src/Board';
+import { assert, expect } from 'chai';
 
-describe('board', () => {
+'use strict';
+
+describe('Board service', () => {
+
   it('should exist', () => {
-    expect(board).to.be.ok;
+    assert.ok(Board);
+  });
+
+  describe('when instantiated', () => {
+
+    it('should create a new board of arbitrary size', () => {
+      let expectedBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+      const boardInstance = new Board(4);
+      let { board } = boardInstance;
+      expect(board).to.deep.equal(expectedBoard);
+    });
+
+    it('should throw if non-number is passed', () => {
+      expect(Board.bind(null, 'foo')).to.throw();
+    });
+
+    it('should throw if number outside min or max bounds', () => {
+      expect(Board.bind(null, 2)).to.throw();
+      expect(Board.bind(null, 12)).to.throw();
+    });
+  });
+
+  describe('getCell', () => {
+
+    it('should return the value of a given cell', () => {
+      let boardInstance = new Board(3);
+      boardInstance.board = [[1, 2, 0], [0, 1, 0], [0, 0, 2]];
+      expect(boardInstance.getCell(1, 1)).to.equal(1);
+      expect(boardInstance.getCell(2, 0)).to.equal(0);
+    });
+  });
+
+  describe('move', () => {
+
+    it('should place a player on the board', () => {
+      let boardInstance = new Board(3);
+      boardInstance.move(1, 1, 2);
+      expect(boardInstance.board[1][1]).to.equal(2);
+    });
+  });
+
+  describe('getEmptyCells', () => {
+
+    it('should return the empty cells', () => {
+      let boardInstance = new Board(3);
+      boardInstance.board = [[1, 2, 0], [0, 1, 0], [0, 0, 2]];
+      let expectedEmpty = [[0, 2], [1, 0], [1, 2], [2, 0], [2, 1]];
+      expect(boardInstance.getEmptyCells()).to.deep.equal(expectedEmpty);
+    });
+  });
+
+  describe('checkWin', () => {
+
+    it('should return winning player, 3 if tie or false if no winner', () => {
+      let boardInstance = new Board(3);
+      boardInstance.board = [[1, 2, 0], [0, 1, 0], [0, 0, 2]];
+      expect(boardInstance.checkWin()).to.equal(false);
+      boardInstance.board = [[2, 2, 2], [1, 1, 0], [0, 0, 1]];
+      expect(boardInstance.checkWin()).to.equal(2);
+      boardInstance.board = [[0, 0, 1], [2, 2, 1], [0, 0, 1]];
+      expect(boardInstance.checkWin()).to.equal(1);
+      boardInstance.board = [[1, 2, 0], [0, 1, 0], [0, 0, 1]];
+      expect(boardInstance.checkWin()).to.equal(1);
+      boardInstance.board = [[1, 2, 1], [2, 1, 2], [1, 2, 2]];
+      expect(boardInstance.checkWin()).to.equal(3);
+    });
   });
 });
